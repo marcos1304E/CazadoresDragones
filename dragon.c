335ba6bfc45d76a217c4b0e391fbf5fa8f6bd989
Lancelot-Mar <cfgs@Info1-Pc02>
2025-02-18 11:16:49 +0100

    PRueba de Leoi y No Iluoiahn

--- dragon.c
+++ dragon.c
@@ -7,8 +7,13 @@
  *  Autor: Leonardo Marescutti, David Castejon y Marcos Escamilla
  */
 
-#define MAX_NOMBRE 20
+#define MAX_NOMBRE 50
+#define MULT_VIDA_DRAGON 2
+
 int MAX_LISTA = 3;
+
+
+//srtuck va en el .h del main
 
 typedef struct{
     int vida;
@@ -18,7 +23,29 @@ typedef struct{
     char nombre[MAX_NOMBRE];
 }Dragon;
 
+void Imprimir_Lista(Dragon * Lista){
+    for(int i = 0; i < MAX_LISTA; i++, Lista++){
+        printf("%d,%d,%d,%.2f,%s\n",Lista->vida,Lista->nivel,Lista->defensa,Lista->oro,Lista->nombre);
+    }
+}
+
+/*
+    ini_dragon(&lista[0],(10*randomnivel[0]), randomnivel[0], (2*randomnivel[0]), (100*randomnivel[0]), "Iulian Dragoi(El renegado)");
+    ini_dragon(&lista[1],(10*randomnivel[0]), randomnivel[1], (2*randomnivel[0]), (100*randomnivel[0]), "Emerson y Jefferson(Dragon bicéfalo)");
+    ini_dragon(&lista[2],(10*randomnivel[0]), randomnivel[2], (2*randomnivel[0]), (1000*randomnivel[0]), "Lory Money(El dragon de oro)");
+
+void ini_dragon_inicial(Dragon *num_dragon, int valor_aleatorio){
+
+    num_dragon -> vida = valor_aleatorio * MULT_VIDA_DRAGON;
+    num_dragon -> nivel = nivel1;
+    num_dragon -> defensa = defensa1;
+    num_dragon -> oro = oro1;
+    strcpy(num_dragon -> nombre, nombre1);
+}
+*/
+
 void ini_dragon(Dragon *num_dragon, int vida1, int nivel1, int defensa1, float oro1, char * nombre1){
+
     num_dragon -> vida = vida1;
     num_dragon -> nivel = nivel1;
     num_dragon -> defensa = defensa1;
@@ -31,8 +58,6 @@ Dragon * añadir_dragon(Dragon * nuevoDragon){
 int vida,nivel,defensa,sum = 1;
 float oro;
 char nombre[MAX_NOMBRE];
-
-    lista_dragon(nuevoDragon);
 
 Dragon * dragon_nuevo = (Dragon*)realloc(nuevoDragon, (MAX_LISTA+sum)*sizeof(Dragon));
    
@@ -57,7 +82,7 @@ Dragon * dragon_nuevo = (Dragon*)realloc(nuevoDragon, (MAX_LISTA+sum)*sizeof(Dra
 
         printf("Nombre del dragon:");
         scanf(" ");
-        fgets(nombre, MAX_LISTA, nombre);
+        fgets(nombre, MAX_LISTA, stdin);
         nombre[strlen(nombre)-1] = '\0';
 
         ini_dragon(&dragon_nuevo[i],vida, nivel, defensa, oro, nombre);
@@ -67,6 +92,8 @@ Dragon * dragon_nuevo = (Dragon*)realloc(nuevoDragon, (MAX_LISTA+sum)*sizeof(Dra
 
     printf("Nueva lista de dragones:\n");
     Imprimir_Lista(nuevoDragon);
+
+    return nuevoDragon;
 }
 
 Dragon * lista_dragon(){
@@ -77,14 +104,12 @@ Dragon * lista_dragon(){
 
     if(lista == NULL){
         printf("ERROR\n");
-        return EXIT_FAILURE;
     }
 
     int * randomnivel = (int*)malloc(MAX_LISTA*sizeof(int));
 
     if(randomnivel == NULL){
         printf("ERROR\n");
-        return EXIT_FAILURE;
     }
 
     srand(time(0));
@@ -97,21 +122,27 @@ Dragon * lista_dragon(){
     }
 
     ini_dragon(&lista[0],(10*randomnivel[0]), randomnivel[0], (2*randomnivel[0]), (100*randomnivel[0]), "Iulian Dragoi(El renegado)");
-    ini_dragon(&lista[1],(10*randomnivel[0]), randomnivel[1], (2*randomnivel[0]), (100*randomnivel[0]), "Emerson y Jefferson(Dragon bicéfalo)");
-    ini_dragon(&lista[2],(10*randomnivel[0]), randomnivel[2], (2*randomnivel[0]), (1000*randomnivel[0]), "Lory Money(El dragon de oro)");
+    ini_dragon(&lista[1],(10*randomnivel[1]), randomnivel[1], (2*randomnivel[1]), (100*randomnivel[1]), "Emerson y Jefferson(Dragon bicéfalo)");
+    ini_dragon(&lista[2],(10*randomnivel[2]), randomnivel[2], (2*randomnivel[2]), (1000*randomnivel[2]), "Lory Money(El dragon de oro)");
+
+    return lista;
 }
 
-void Imprimir_Lista(Dragon * Lista){
 
-    for(int i = 0; i < MAX_LISTA; i++, Lista++){
-    printf("%d,%d,%d,%f,%s,",Lista->vida,Lista->nivel,Lista->defensa,Lista->oro,Lista->nombre);
+int main() {
+    Dragon *lista = lista_dragon();
+    if (lista == NULL) {
+        return EXIT_FAILURE;
     }
-}
 
-int main(){
+    printf("Lista inicial de dragones:\n");
+    Imprimir_Lista(lista);
 
-    
-    
+    lista = añadir_dragon(lista);
+    if (lista == NULL) {
+        return EXIT_FAILURE;
+    }
 
+    free(lista);
     return 0;
 }
