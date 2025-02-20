4c4aac6f72fc52314d4063e483dfbfdac794bf3e
Lancelot-Mar <cfgs@Info1-Pc02>
2025-02-20 14:26:22 +0100

    combate.c_terminada estructura

--- combate.c
+++ combate.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>+
+#include <string.h>
 #include <time.h>
 
-#include <cazador.h>
-#include <dragon.h>
+#include "cazador.h"
+#include "dragon.h"
 
 
 /*
  *  Autor: Leonardo Marescutti, David Castejon y Marcos Escamilla
  */
+
+int ataque_dragon(){
+
+    //ataques
+        //ataque basico+nivel (con posibilidad de critico)
+        //cada 3 turnos ataque especial
+}
+
+int ataque_cazador(){
+
+    //ataques
+        //ataque basico
+        //cada 1 turnos ataque especial
+
+    //items (con posibilidad de critico)
+        //despliegue objetos
+        //seleccionar objeto con swich (1,2,3,4,5...)
+}
 
 int turnos(){
 
@@ -20,6 +38,12 @@ int turnos(){
         //ataque dragon(turno)
 
     }while(/*Vida de Cazador != 0*/||/*Vida de Dragon != 0*/);
+
+    if (/*Vida de Cazador == 0*/){
+
+        //llamar (utilidades())
+
+    }
 
     return ;
 }
